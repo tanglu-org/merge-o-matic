96619ea0781d9f5d1c2432bac67cba5fd762beef
Scott James Remnant <scott@netsplit.com>
2008-04-28 20:05:17 +0100

    add tool to pack the old stuff

---
+++ pack-archive.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+umask 002
+
+cd /srv/patches.ubuntu.com
+
+find changes -name "*.changes" -mtime +182 -print0 | xargs -0r bzip2
+find diffs -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2
+find patches -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2
