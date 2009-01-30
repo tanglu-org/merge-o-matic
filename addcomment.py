78fb6978361d047812509a9bf68d8450e5ed2842
Adrien Cunin <adri2000@ubuntu.com>
2009-01-30 22:30:43 +0100

    addcomment.py: explicitely import cgi.escape and mention the name of the parameter quote when calling escape()

--- addcomment.py
+++ addcomment.py
@@ -1,13 +1,14 @@
 <%
 from momlib import *
 import fcntl
+from cgi import escape
 
 def add_comment(package, comment, file):
     """Add a comment to the comments file"""
     file_comments = open(file, "a")
     fcntl.lockf(file_comments, fcntl.LOCK_EX)
     the_comment = comment.replace("\n", " ")
-    the_comment = escape(the_comment[:100], True)
+    the_comment = escape(the_comment[:100], quote=True)
     file_comments.write("%s: %s\n" % (package, the_comment))
     file_comments.close()
 
