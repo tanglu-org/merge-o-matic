82b2da929e8edc1220824b1aca274e72d6b6f799
Scott James Remnant <scott@netsplit.com>
2009-03-13 12:49:35 +0000

    Adrien Cunin <adri2000@gmail.com>
    Currently addcomment.py doesn't check that the parameters package,
    comment and component exist. That's why calling addcomment.py without
    any parameter returns an internal server error.  Instead of that, it
    should check that package and comment exist, otherwise print an error,
    and if component exist, redirect, otherwise print a success message. See
    attached patch.

--- addcomment.py
+++ addcomment.py
@@ -2,6 +2,12 @@
 from momlib import *
 from cgi import escape
 
-add_comment(req.form["package"], req.form["comment"])
-util.redirect(req, req.form["component"]+".html")
+if req.form.has_key("package") and req.form.has_key("comment"):
+    add_comment(req.form["package"], req.form["comment"])
+    if req.form.has_key("component"):
+        util.redirect(req, req.form["component"]+".html")
+    else:
+        req.write("Comment added.")
+else:
+    req.write("I need at least two parameters: package and comment. component is optional.")
 %>
