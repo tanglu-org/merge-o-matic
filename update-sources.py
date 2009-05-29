9152623c6cea9ff5863a0d7ff9691a3305c012b3
Scott James Remnant <scott@netsplit.com>
2009-05-29 09:23:15 +0200

    missing distro support

--- update-sources.py
+++ update-sources.py
@@ -31,7 +31,11 @@ def main(options, args):
 
     # Iterate the pool directory of the given distributions
     for distro in distros:
-        for hpart in os.listdir("%s/pool/%s" % (ROOT, distro)):
+        try:
+            hparts = os.listdir("%s/pool/%s" % (ROOT, distro))
+        except OSError:
+            continue
+        for hpart in hparts:
             for package in os.listdir("%s/pool/%s/%s" % (ROOT, distro, hpart)):
                 update_pool_sources(distro, package)
 
