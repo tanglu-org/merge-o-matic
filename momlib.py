d0d2b6d4a6b1856d36e25f0a57ab6d07c91e44f6
Colin Watson <cjwatson@canonical.com>
2010-10-11 13:42:33 +0100

    switch to natty

--- momlib.py
+++ momlib.py
@@ -48,7 +48,7 @@ ROOT = "/srv/patches.ubuntu.com"
 DISTROS = {
     "ubuntu": {
         "mirror": "http://archive.ubuntu.com/ubuntu",
-        "dists": [ "maverick" ],
+        "dists": [ "natty" ],
         "components": [ "main", "restricted", "universe", "multiverse" ],
         "expire": True,
         },
@@ -86,7 +86,7 @@ DISTROS = {
 
 # Destination distribution and release
 OUR_DISTRO = "ubuntu"
-OUR_DIST   = "maverick"
+OUR_DIST   = "natty"
 
 # Default source distribution and release
 SRC_DISTRO = "debian"
