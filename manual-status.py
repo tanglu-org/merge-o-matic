1e58aaceae599f76488d571cf0647a17feea8f78
Colin Watson <cjwatson@canonical.com>
2010-12-15 14:17:57 +0000

    fix spacing

--- manual-status.py
+++ manual-status.py
@@ -312,31 +312,31 @@ def write_status_json(component, merges, left_distro, right_distro):
         for section in SECTIONS:
             for uploaded, priority, package, user, uploader, source, \
                     left_version, right_version in merges:
-                print >>status, ' { ',
+                print >>status, ' {',
                 # source_package, short_description, and link are for
                 # Harvest (http://daniel.holba.ch/blog/?p=838).
-                print >>status, '"source_package": "%s", ' % package,
-                print >>status, '"short_description": "merge %s", ' % right_version,
-                print >>status, '"link": "https://merges.ubuntu.com/%s/%s/", ' % (pathhash(package), package),
-                print >>status, '"section": "%s", ' % section
-                print >>status, '"uploaded": %s, ' % ("true" if uploaded else "false"),
-                print >>status, '"priority": "%s", ' % priority,
+                print >>status, '"source_package": "%s",' % package,
+                print >>status, '"short_description": "merge %s",' % right_version,
+                print >>status, '"link": "https://merges.ubuntu.com/%s/%s/",' % (pathhash(package), package),
+                print >>status, '"section": "%s",' % section,
+                print >>status, '"uploaded": %s,' % ("true" if uploaded else "false"),
+                print >>status, '"priority": "%s",' % priority,
                 if user is not None:
                     who = user
                     who = who.replace('\\', '\\\\')
                     who = who.replace('"', '\\"')
-                    print >>status, '"user": "%s", ' % who,
+                    print >>status, '"user": "%s",' % who,
                     if uploader is not None:
                         u_who = uploader
                         u_who = u_who.replace('\\', '\\\\')
                         u_who = u_who.replace('"', '\\"')
-                        print >>status, '"uploader": "%s", ' % u_who,
+                        print >>status, '"uploader": "%s",' % u_who,
                 binaries = source["Binary"].split(', ')
-                print >>status, '"binaries": [ %s ], ' % \
+                print >>status, '"binaries": [ %s ],' % \
                                 ', '.join(['"%s"' % b for b in binaries]),
-                print >>status, '"left_version": "%s", ' % left_version,
+                print >>status, '"left_version": "%s",' % left_version,
                 print >>status, '"right_version": "%s"' % right_version,
-                print >>status, ' }'
+                print >>status, '}'
         print >>status, ']'
     finally:
         status.close()
