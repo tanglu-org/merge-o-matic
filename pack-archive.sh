2a231fa5733e0a0142c475940441433a7f2fd87d
Colin Watson <cjwatson@canonical.com>
2012-01-30 17:19:42 +0000

    pack-archive.sh: pack *.slipped-patch files under patches as well

--- pack-archive.sh
+++ pack-archive.sh
@@ -7,4 +7,4 @@ cd /srv/patches.ubuntu.com
 
 find changes -name "*.changes" -mtime +182 -print0 | xargs -0r bzip2
 find diffs -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2
-find patches -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2
+find patches \( -name "*.patch" -o -name "*.slipped-patch" \) -mtime +182 -print0 | xargs -0r bzip2
